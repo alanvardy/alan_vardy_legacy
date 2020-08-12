c38b7d957bd139a8cb3c7095fe2ffc0ccb5e62f9
Alan Vardy <alanvardy@gmail.com>
2020-08-11 18:56:31 -0700

    Update dependencies

--- .check.exs
+++ .check.exs
@@ -15,15 +15,16 @@
     ## custom new tools may be added (mix tasks or arbitrary commands)
     # {:my_mix_check, command: "mix release", env: %{"MIX_ENV" => "prod"}},
     # {:my_arbitrary_check, command: "npm test", cd: "assets"},
-
-    {:cypress, command: "mix cypress.run"},
+    {:format, command: "mix format", exit_status: false},
+    {:credo, command: "mix credo --strict", deps: [:format]},
+    {:cypress, command: "mix cypress.run", deps: [:format, :credo]},
     {:ex_coveralls,
      command: "mix coveralls.html",
      require_files: ["test/test_helper.exs"],
-     env: %{"MIX_ENV" => "test"}},
-    {:sobelow, command: "mix sobelow --config"},
-    {:credo, command: "mix credo --strict"},
-    {:ex_unit, command: "mix test", env: %{"MIX_ENV" => "test"}}
+     env: %{"MIX_ENV" => "test"},
+     deps: [:format, :credo]},
+    {:sobelow, command: "mix sobelow --config", deps: [:format, :credo]},
+    {:ex_unit, command: "mix test", env: %{"MIX_ENV" => "test"}, deps: [:format, :credo]}
 
     # {:my_arbitrary_script, command: ["my_script", "argument with spaces"], cd: "scripts"}
   ]
